fbab3000c792c61b25aaafc2e5a5eef2b8ec21ba
WhyFld <yannis@getamphi.com>
2026-02-28 23:55:08 +0000

    Update embed.js

--- embed.js
+++ embed.js
@@ -78,7 +78,7 @@ function createMinimizedIcon() {
     script.onload = () => {
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000);
-      camera.position.z = 3;
+      camera.position.z = 4.5;
 
       const renderer = new THREE.WebGLRenderer({ 
         canvas: canvas, 
